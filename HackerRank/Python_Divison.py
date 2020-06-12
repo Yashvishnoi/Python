1cedbc97f66ef5730fdd2278e8bce4ed41f20b06
Yash Vishnoi <jivishnoi@gmail.com>
2020-06-12 14:56:04 +0530

    HackerRank solution

---
+++ HackerRank/Python_Divison.py
@@ -0,0 +1,11 @@
+# Read two integers and print two lines. The first line should contain integer
+# division. The second line should contain float division.
+#You don't need to perform any rounding or formatting operations.
+ # ================================================================
+
+a = int(input())
+b = int(input())
+Div= int(a/b)
+print(Div)
+divison=float(a/b)
+print(divison)
