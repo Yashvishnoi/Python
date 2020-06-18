1165d21f1b19ce006a34306e59fc85dd3cc10952
Yash Vishnoi <jivishnoi@gmail.com>
2020-06-18 15:07:42 +0530

    Comment , Escape and sequence

---
+++ Comment_Escape_sequence.py
@@ -0,0 +1,6 @@
+"""
+This is Harry with code (Multiple line comment)
+"""
+print("This is harry bawa. ",end="")
+print("He providing course with free of cost",end=",")
+print("\tHe is a growing youtuber")
