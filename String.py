2f0b7298e5ebe418d44f77646d8d0c89b761af64
Yash Vishnoi <jivishnoi@gmail.com>
2020-06-19 15:50:37 +0530

    String and string function

---
+++ String.py
@@ -0,0 +1,16 @@
+mystr = "harry is A coder"
+print(mystr)
+print(len(mystr))
+print(mystr[0:5])
+print(mystr[:5])
+print(mystr[0:])
+print(mystr[::3]) # it will escape 3 characters
+print(mystr[-4:-2]) # it will start counting from right hand side
+print(mystr[::-1]) # it will reverse the string
+print(mystr. endswith("boy")) # it will give true or false
+print(mystr. count("r")) # it will count the number of r's
+print(mystr.capitalize()) # it will capital the first  letter of strings
+print(mystr.find("is")) # it find the index of the is
+print(mystr.lower()) # it will lower the strings
+print(mystr.upper()) # it will upper the strings
+print(mystr.replace ("is","are")) # it will replace the is strings to are
