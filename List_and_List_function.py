a53f94750165f047e33157fecdefbcff7c436558
Yash Vishnoi <jivishnoi@gmail.com>
2020-06-19 22:16:45 +0530

    List and its function

---
+++ List_and_List_function.py
@@ -0,0 +1,31 @@
+grocery = "Harpic", "Deo", "Soap"
+print(grocery)
+print(grocery[0])
+numbers= [1,8,5,96,24,75]
+numbers.sort() # It will sort the list
+print(numbers)
+numbers.reverse() # It will reverse the string
+print(numbers)
+print(numbers[2:5])# it will print after 2 numbers to 5 (count will start from 1)
+print(numbers)
+print(numbers[::2]) # it will print escape one letter and print another
+print(numbers[::3]) # it will print escape two letter and print another
+print(numbers[::-1]) # it will reverse the string]
+print(max(numbers))# it will give the maximum number
+print(min(numbers))# it will give the minimum number
+numbers.append(7) # it will add the number 7 in end
+print(numbers)
+numbers.insert(1, 51) # it will show at 1th  index you want to add the number 51
+print(numbers)
+numbers.remove(96) # it will remove the number 96 from the list
+print(numbers)
+numbers.pop() # it will remove the last element
+print(numbers)
+numbers[1]=98 # it will change the number from index 1
+print(numbers)
+tp=(1,8,89 ) # it indicates tupple whoes value cannot change
+print(tp)
+a=10
+b=20
+a,b = b,a # it will interchange the a and b
+print(a,b)
