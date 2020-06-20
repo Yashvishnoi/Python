9b1cc53b749f32507c218307c7b837047f9fcba9
Yash Vishnoi <jivishnoi@gmail.com>
2020-06-20 16:24:28 +0530

    If else loop

---
+++ If_else.py
@@ -0,0 +1,11 @@
+list1 = [1,5,8]
+if 57 in list1 :
+    print("Ya, It is present")
+else :
+    print("No, It is not present")
+print("What is your Age")
+a = int(input())
+if  a > 18:
+    print("You are eligible drive")
+else:
+    print("You are not eligible to drive")
