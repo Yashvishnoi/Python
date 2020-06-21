4642976c7752973a76c320216e3a681eb7317c19
Yash Vishnoi <jivishnoi@gmail.com>
2020-06-21 16:56:34 +0530

    for loop iteration

---
+++ Loops/For_Loops.py
@@ -0,0 +1,10 @@
+# For Loops
+list1 = ["Harry", "Carry", "Nikhil", "Flying Beast","Mortal"]
+for item in list1 : # it will print all items in list
+    print(item)
+list1 = (["Harry",745], ["Carry",78], ["Nikhil",5], ["Flying Beast",8],["Mortal",4])
+for item in list1 :
+    print(item)
+list1 = (["Harry",745], ["Carry",78], ["Nikhil",5], ["Flying Beast",8],["Mortal",4])
+for item,toffee in list1 :
+    print(item,toffee)
