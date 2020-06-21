2149c62f02ad8c8752c6b0046058cfb3bc3940f1
Yash Vishnoi <jivishnoi@gmail.com>
2020-06-21 17:01:51 +0530

    Update the line 10

--- Loops/For_Loops.py
+++ Loops/For_Loops.py
@@ -6,5 +6,5 @@ list1 = (["Harry",745], ["Carry",78], ["Nikhil",5], ["Flying Beast",8],["Mortal"
 for item in list1 : # it will print both thing as one
     print(item)
 list1 = (["Harry",745], ["Carry",78], ["Nikhil",5], ["Flying Beast",8],["Mortal",4])
-for item,toffee in list1 : # it will print both object as different because both are reffered by different varia 
-    print(item,toffee)
+for item,toffee in list1 : # it will print both object as different because both are reffered by different varia
+    print(item," And they eat no. of toffee : ",toffee)
