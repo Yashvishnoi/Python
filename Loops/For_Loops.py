25512a33ceb45622af8c3ad71f5a2ff7a0644081
Yash Vishnoi <jivishnoi@gmail.com>
2020-06-21 17:00:15 +0530

    For loops iteration set inside set through two different object

--- Loops/For_Loops.py
+++ Loops/For_Loops.py
@@ -5,3 +5,6 @@ for item in list1 : # it will print all items in list
 list1 = (["Harry",745], ["Carry",78], ["Nikhil",5], ["Flying Beast",8],["Mortal",4])
 for item in list1 : # it will print both thing as one
     print(item)
+list1 = (["Harry",745], ["Carry",78], ["Nikhil",5], ["Flying Beast",8],["Mortal",4])
+for item,toffee in list1 : # it will print both object as different because both are reffered by different varia 
+    print(item,toffee)
