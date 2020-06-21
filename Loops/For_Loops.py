1d3071b3971120f706074ee30a6d24833da7e801
Yash Vishnoi <jivishnoi@gmail.com>
2020-06-21 16:58:28 +0530

    For loops iteration normally

--- Loops/For_Loops.py
+++ Loops/For_Loops.py
@@ -3,8 +3,5 @@ list1 = ["Harry", "Carry", "Nikhil", "Flying Beast","Mortal"]
 for item in list1 : # it will print all items in list
     print(item)
 list1 = (["Harry",745], ["Carry",78], ["Nikhil",5], ["Flying Beast",8],["Mortal",4])
-for item in list1 :
+for item in list1 : # it will print both thing as one
     print(item)
-list1 = (["Harry",745], ["Carry",78], ["Nikhil",5], ["Flying Beast",8],["Mortal",4])
-for item,toffee in list1 :
-    print(item,toffee)
