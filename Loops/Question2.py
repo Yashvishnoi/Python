3a05cd368ecedfb66a9721cf7775a5b02e6d7e69
Yash Vishnoi <jivishnoi@gmail.com>
2020-06-22 23:33:21 +0530

    Done with question2

---
+++ Loops/Question2.py
@@ -0,0 +1,7 @@
+while (1):
+     Input_Number = int(input("Enter the number"))
+     if Input_Number > 100 :
+         print("Congratulation, you enetered a number which is greater than 100")
+         break
+     else :
+        print(Input_Number)
