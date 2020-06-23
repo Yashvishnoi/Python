01f6ccf48db6084e9eed8c3a7768afdde9d85d1c
Yash Vishnoi <jivishnoi@gmail.com>
2020-06-23 13:48:31 +0530

    Done with logical operator

--- Operators.py
+++ Operators.py
@@ -1,3 +1,5 @@
+ # The below operator is Arithmetic operators
+
 print("5+6 is :" ,5+6)
 print("5-6 is :" ,5-6)
 print("5*6 is :",5*6)
@@ -6,5 +8,9 @@ print("6//5 is :",6//5) # it will give the integer value
 print("6**5 is :",6**5) # Operator is called double expotential operator it will rais ethe power
 print("6%5 is :",6%5) # it will give the remainder
 
- # The above operator is Arithmetic operators
+# The below operator is Logical Operator
+a=True
+b=False
+print(a or b)
+print(a and b)
  
