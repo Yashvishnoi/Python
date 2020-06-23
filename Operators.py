6dacf34eb017de6a6d300f94999e7cf706420e1e
Yash Vishnoi <jivishnoi@gmail.com>
2020-06-23 13:34:18 +0530

    Done with assignment operator

---
+++ Operators.py
@@ -0,0 +1,10 @@
+print("5+6 is :" ,5+6)
+print("5-6 is :" ,5-6)
+print("5*6 is :",5*6)
+print("6/5 is :",6/5)
+print("6//5 is :",6//5) # it will give the integer value
+print("6**5 is :",6**5) # Operator is called double expotential operator it will rais ethe power
+print("6%5 is :",6%5) # it will give the remainder
+
+ # The above operator is Arithmetic operators
+ 
