514d5654d24339effac809861492cfbcc70b26e6
Yash Vishnoi <jivishnoi@gmail.com>
2020-07-12 22:18:24 +0530

    Added the break statement

--- Loops/Break_and_continue.py
+++ Loops/Break_and_continue.py
@@ -1,4 +1,6 @@
 i=0
-while(i<45):
+while(True): # while(1) it will make the loop permanent to execute
     print(i+1, end=" ") # It will print number from 1 to 45
+    if(i==44) :
+        break
     i = i+1
