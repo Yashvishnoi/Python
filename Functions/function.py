ae21e74b0a2287d3924a2c1d645fd3a67ad8969a
Yash Vishnoi <jivishnoi@gmail.com>
2020-07-13 15:11:53 +0530

    Average function added

--- Functions/function.py
+++ Functions/function.py
@@ -1,7 +1,12 @@
 def Sum(a,b) :
     c=a+b
     return c
-
-
-d=Sum(2,5)
+def Average(a,b):
+    c= (a+b)/2
+    return c
+a=int(input("Enter a value of a: "))
+b=int(input("Enter a value of b: "))
+d=Sum(a,b)
 print(d)
+e=Average(a,b)
+print (e)
