2a66b693658bd2eae9d798da88c4422dde3eedf1
Yash Vishnoi <jivishnoi@gmail.com>
2020-07-14 13:05:03 +0530

    Added line 8

--- Try_Except_Excption_handling.py
+++ Try_Except_Excption_handling.py
@@ -1,7 +1,8 @@
-num1= input()
-num2=input()
+num1= input("Enter num1 ")
+num2=input("Enter num2 ")
 
 try:
     print("The sum of two number is" , int(num1)+int(num2))
 except Exception as e:
     print(e)
+print("This line is very important")
