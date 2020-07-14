f8e761632e999847f0c979c68214fc4d834e2670
Yash Vishnoi <jivishnoi@gmail.com>
2020-07-14 15:23:07 +0530

    Modified line 5, 12, 19, 25

--- files/Writing_and_Appending_to_a_file.py
+++ files/Writing_and_Appending_to_a_file.py
@@ -1,3 +1,9 @@
+print("To write to a file")
 f=open("harry2.txt","w")
-f.write("Harry bhai is villan for coding")
-f.close
+f.write("Harry bhai is villan for coding. ")
+f.close()
+
+print("To append to file ")
+f=open("harry2.txt","a")
+f.write(" He loves coding.")
+f.close()
