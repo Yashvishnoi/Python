44362d7813181cf160743885dcfe733a2f6d0852
Yash Vishnoi <jivishnoi@gmail.com>
2020-07-14 14:19:12 +0530

    f.read(3) function added

--- files/Open_Read_Readline.py
+++ files/Open_Read_Readline.py
@@ -2,3 +2,15 @@ f= open("harry.txt","rt") # read file in text mode by default
 content = f.read()
 print(content)
 f.close
+
+# read file in bnary mode
+f= open("harry.txt","rb")
+content = f.read()
+print(content)
+f.close
+
+# f.read(3) it will read 3 characters
+f= open("harry.txt","rt")
+content = f.read(3)
+print(content)
+f.close
