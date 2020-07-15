70d48830cd1833251d371739e18d7f931dc99e5b
Yash Vishnoi <jivishnoi@gmail.com>
2020-07-15 14:37:14 +0530

    Global variables

---
+++ Scope_GlobalVariables_and_GlobalKeyword.py
@@ -0,0 +1,9 @@
+l =10; # Global
+def function1(n):
+    l=5
+    m=8
+    print("Here inside the block the value of l is ",l)
+    print(n,"I have printed")
+
+function1("This is me")
+print("Here outside the block the value of l is ",l)
