9a08fb0db42dd482ab94bbebd2dc6df4cf159ac5
Yash Vishnoi <jivishnoi@gmail.com>
2020-07-16 22:37:37 +0530

    Modified the line 9

--- Operators.py
+++ Operators.py
@@ -6,7 +6,7 @@ print("5*6 is :",5*6)
 print("6/5 is :",6/5)
 print("6//5 is :",6//5) # it will give the integer value
 print("6**5 is :",6**5) # Operator is called double expotential operator it will rais ethe power
-print("6%5 is :",6%5) # it will give the remainder
+print("6%5 is :",60%57) # it will give the remainder
 
 # The below operator is Logical Operator
 a=True
