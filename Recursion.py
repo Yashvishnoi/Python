04e85c0138e60ca5a56044222a38e01444306ad7
Yash Vishnoi <jivishnoi@gmail.com>
2020-07-18 17:22:31 +0530

    Recursion using iteration

---
+++ Recursion.py
@@ -0,0 +1,9 @@
+# Iterative method to find the factorial
+def factorial(n):
+    fac= 1
+    for i in range (n):
+        fac= fac*(i+1)
+    return fac
+
+num= int(input("Enter the number "))
+print(factorial(num))
