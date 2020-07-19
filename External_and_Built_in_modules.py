c0931b2f2563d9e2fa0321ef81dfc2c367db6f71
Yash Vishnoi <jivishnoi@gmail.com>
2020-07-19 22:37:18 +0530

    Return the next random floating point number in the range [0.0, 1.0)

--- External_and_Built_in_modules.py
+++ External_and_Built_in_modules.py
@@ -1,4 +1,9 @@
-import random 
+import random
 
+# Return a random integer N such that a <= N <= b. Alias for randrange(a, b+1).
 random_number = random.randint(0,1)
 print(random_number)
+
+# Return the next random floating point number in the range [0.0, 1.0)
+rand=random.random()*100
+print(rand)
