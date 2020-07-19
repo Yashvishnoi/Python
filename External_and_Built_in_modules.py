bb36ac67a50964ac2b661c707711f7132a81e1ac
Yash Vishnoi <jivishnoi@gmail.com>
2020-07-19 22:48:42 +0530

    # Single random element from a sequence

--- External_and_Built_in_modules.py
+++ External_and_Built_in_modules.py
@@ -7,3 +7,8 @@ print(random_number)
 # Return the next random floating point number in the range [0.0, 1.0)
 rand=random.random()*100
 print(rand)
+
+# # Single random element from a sequence
+Ist = ["Star Plus","DD1" , "Aaj Tak","CodeWithHarry"]
+choice = random.choice(Ist)
+print(choice)
