d35c79d14b93b40611b73e36e50494e0cdc969cc
Yash Vishnoi <jivishnoi@gmail.com>
2020-07-19 14:24:47 +0530

    Added line 9 & 10

--- fibonacci.py
+++ fibonacci.py
@@ -1,8 +1,10 @@
 def fibonacci(n):
-    a=1 ,b =2
     if n==1:
         return 0
     elif n==2 :
         return 1
     else :
          return fibonacci(n-1) + fibonacci(n-2)
+
+n= int(input("Enter a number "))
+print(fibonacci(n))
