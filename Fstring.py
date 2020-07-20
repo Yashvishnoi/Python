f58090ff660d8a986bf39097e943637320088b08
Yash Vishnoi <jivishnoi@gmail.com>
2020-07-20 13:13:13 +0530

    Using fstring

--- Fstring.py
+++ Fstring.py
@@ -1,6 +1,7 @@
 me = "Yash"
 a1 = 3
 # a= "This is %s %s "%(me, a1)
-a= "This is {1} {0} "
-b=a.format(me,a1)
-print(b)
+#a= "This is {1} {0} "
+#b=a.format(me,a1)
+a= f"this is a {a1} {me} {a1}"
+print(a)
