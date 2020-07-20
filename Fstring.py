5c0ca5a01302f9caf9d40e5f9c24843b153a9a3a
Yash Vishnoi <jivishnoi@gmail.com>
2020-07-20 13:27:06 +0530

    Added two more function

--- Fstring.py
+++ Fstring.py
@@ -1,7 +1,8 @@
+import math
 me = "Yash"
 a1 = 3
 # a= "This is %s %s "%(me, a1)
 #a= "This is {1} {0} "
 #b=a.format(me,a1)
-a= f"this is a {a1} {me} {a1}"
+a= f"this is a {a1} {me} {a1} {4*65} {math.tan(45)}"
 print(a)
